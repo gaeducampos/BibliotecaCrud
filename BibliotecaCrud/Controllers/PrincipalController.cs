438a83ace6b557d7d35724b54108548055efa14b
gaeducampos <gaeducampos@gmail.com>
2022-05-31 18:32:08 -0600

    login done, register done

--- BibliotecaCrud/Controllers/PrincipalController.cs
+++ BibliotecaCrud/Controllers/PrincipalController.cs
@@ -3,6 +3,7 @@ using BibliotecaCrud.Data;
 using BibliotecaCrud.Models;
 using System.Web;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace BibliotecaCrud.Controllers
 {
@@ -32,16 +33,17 @@ namespace BibliotecaCrud.Controllers
 
         public IActionResult Register(User userObject) 
         {
-            
+            Regex validateEmailRegex = new Regex("^\\S+@\\S+\\.\\S+$");
 
-            if(userObject.Password == userObject.ConfirmPassword)
+
+            if (userObject.Password == userObject.ConfirmPassword && validateEmailRegex.IsMatch(userObject.Email))
             {
                 userObject.Password = sha256Object.convertToSha256(userObject.Password);
 
 
             } else
             {
-                ViewData["Message"] = "Las contrasenas no son iguales";
+                ViewData["Message"] = "Las contrasenas no son iguales o el correo no es valido";
                 return View();
             }
 
--- BibliotecaCrud/Models/User.cs
+++ BibliotecaCrud/Models/User.cs
@@ -1,12 +1,16 @@
-﻿namespace BibliotecaCrud.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaCrud.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Correo es obligatorio")]
         public string Email { get; set; }
-
+        [Required(ErrorMessage = "El campo Contraseña es obligatorio")]
         public string Password { get; set; }
-
+        [Required(ErrorMessage = "El campo Confirmar Contraseña es obligatorio")]
         public string ConfirmPassword { get; set; }
 
     }
